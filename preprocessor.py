f487329f37d859c4a4837ffa5278097dd203a907
n09d00 <namanh.do@stud.tu-darmstadt.de>
2024-08-31 21:31:39 +0200

    - added prototype for preprocessor
    - added input and output file of preprocessor

--- preprocessor.py
+++ preprocessor.py
@@ -1,4 +1,6 @@
 import re
+import chess.pgn
+import os
 
 # takes PGN files of chess games and outputs the ELO of White, Black and the moves that were played
 # filters every game that does not contain either whit's or black's ELO
@@ -19,3 +21,43 @@ def preprocess(file, output_file):
     # Write the filtered lines to a new file
     with open(output_file, 'w') as output_file:
         output_file.write('\n'.join(matches))
+
+
+def preprocess_data(input_file, output_file):
+    # open input file to read and output file to write
+    pgn_file = open(input_file)
+    curr_game = chess.pgn.read_game(pgn_file)
+
+    # create and open the output file
+    with open(output_file, 'w') as out_file:
+        #i = 0
+        while curr_game is not None:
+            # check if the game has a variant
+            cond1 = "Variant" not in curr_game.headers.keys()
+            # check if the game ended due to time limit
+            cond2 = curr_game.headers["Termination"] != "Time forfeit"
+
+            if cond1 and cond2:
+                # gather all information for output file
+                elo_w_val = curr_game.headers["WhiteElo"]
+                elo_b_val = curr_game.headers["BlackElo"]
+                res = curr_game.headers["Result"]
+                moves = curr_game.mainline_moves().__str__().replace('#', '')
+
+                elo_w = f"[WhiteElo \"{elo_w_val}\"]"
+                elo_b = f"[BlackElo \"{elo_b_val}\"]"
+
+                # write entries to file
+                out_file.write(elo_w + "\n")
+                out_file.write(elo_b + "\n")
+                out_file.write("\n")
+                out_file.write(moves + " " + res + "\n")
+
+            #if i == 20:
+            #    break
+
+            #i += 1
+            curr_game = chess.pgn.read_game(pgn_file)
+
+if __name__ == "__main__":
+    preprocess_data("data/lichess_db_standard_rated_2013-01.pgn", "data/output.pgn")
